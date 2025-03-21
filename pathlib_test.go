ab481a9fb7900fc3395c8129ec02c830f68fd141
jeftadlvw <143009333+jeftadlvw@users.noreply.github.com>
2025-03-21 19:01:32 +0100

    ✅ update tests; use testify/require

--- pathlib_test.go
+++ pathlib_test.go
@@ -4,9 +4,8 @@ import (
 	"encoding"
 	"encoding/json"
 	"fmt"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"os"
-	"path/filepath"
 	"strings"
 	"testing"
 )
@@ -33,12 +32,12 @@ func TestPathInterfaceImplementations(t *testing.T) {
 
 	t.Run("TextMarshaler", func(t *testing.T) {
 		_, ok := interface{}(path).(encoding.TextMarshaler)
-		assert.True(t, ok)
+		require.True(t, ok)
 	})
 
 	t.Run("TextUnMarshaler", func(t *testing.T) {
 		_, ok := interface{}(path).(encoding.TextUnmarshaler)
-		assert.True(t, ok)
+		require.True(t, ok)
 	})
 }
 
@@ -96,27 +95,27 @@ func TestPathConversions(t *testing.T) {
 		inputPath := NewPath(input)
 
 		t.Run("fromString", func(t *testing.T) {
-			assert.Equal(t, *expect, *inputPath)
+			require.Equal(t, *expect, *inputPath)
 		})
 
 		t.Run("toString", func(t *testing.T) {
-			assert.Equal(t, expect.path, inputPath.String())
+			require.Equal(t, expect.path, inputPath.String())
 		})
 
 		t.Run("text unmarshalling", func(t *testing.T) {
 			var emptyPath = &Path{}
 
 			err := emptyPath.UnmarshalText([]byte(input))
-			assert.NoError(t, err)
+			require.NoError(t, err)
 
-			assert.Equal(t, *expect, *emptyPath)
+			require.Equal(t, *expect, *emptyPath)
 		})
 
 		t.Run("text marshalling", func(t *testing.T) {
 			marshaled, err := inputPath.MarshalText()
-			assert.NoError(t, err)
+			require.NoError(t, err)
 
-			assert.Equal(t, expect.String(), string(marshaled))
+			require.Equal(t, expect.String(), string(marshaled))
 		})
 
 		t.Run("json unmarshalling", func(t *testing.T) {
@@ -124,17 +123,17 @@ func TestPathConversions(t *testing.T) {
 			input := fmt.Sprintf(`["%s"]`, inputPath.String())
 
 			err := json.Unmarshal([]byte(input), &emptyPaths)
-			assert.NoError(t, err)
+			require.NoError(t, err)
 
-			assert.Len(t, emptyPaths, 1)
-			assert.Equal(t, *expect, *emptyPaths[0])
+			require.Len(t, emptyPaths, 1)
+			require.Equal(t, *expect, *emptyPaths[0])
 		})
 
 		t.Run("json marshalling", func(t *testing.T) {
 			marshaled, err := json.Marshal([]*Path{inputPath})
-			assert.NoError(t, err)
+			require.NoError(t, err)
 
-			assert.Equal(t, fmt.Sprintf(`["%s"]`, expect.String()), string(marshaled))
+			require.Equal(t, fmt.Sprintf(`["%s"]`, expect.String()), string(marshaled))
 		})
 	})
 }
@@ -150,18 +149,18 @@ func TestPathWhiteSpaceRepresentation(t *testing.T) {
 	}
 
 	runForResults(t, cases, func(t *testing.T, input string, expect []string) {
-		assert.Equal(t, len(expect), 2)
+		require.Equal(t, len(expect), 2)
 		internalRepr := expect[0]
 		stringRepr := expect[1]
 
 		inputPath := NewPath(input)
 
 		t.Run("fromString", func(t *testing.T) {
-			assert.Equal(t, internalRepr, inputPath.path)
+			require.Equal(t, internalRepr, inputPath.path)
 		})
 
 		t.Run("toString", func(t *testing.T) {
-			assert.Equal(t, stringRepr, inputPath.String())
+			require.Equal(t, stringRepr, inputPath.String())
 		})
 	})
 }
@@ -169,26 +168,26 @@ func TestPathWhiteSpaceRepresentation(t *testing.T) {
 func TestNewCwd(t *testing.T) {
 	// call library function
 	pathlibCwdPath, err := NewCwd()
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
 	// recreate cwd path using stdlib
 	localCwd, err := os.Getwd()
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	localCwdPath := NewPath(localCwd)
 
 	// assert
-	assert.Equal(t, localCwdPath, pathlibCwdPath)
+	require.Equal(t, localCwdPath, pathlibCwdPath)
 }
 
 func TestNewHome(t *testing.T) {
 	pathlibHomePath, err := NewHome()
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
 	localHome, err := os.UserHomeDir()
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	localHomePath := NewPath(localHome)
 
-	assert.Equal(t, localHomePath, pathlibHomePath)
+	require.Equal(t, localHomePath, pathlibHomePath)
 }
 
 func TestPathFromParts(t *testing.T) {
@@ -205,47 +204,7 @@ func TestPathFromParts(t *testing.T) {
 	}
 
 	runForResults(t, cases, func(t *testing.T, input []string, expect *Path) {
-		assert.Equal(t, *expect, *PathFromParts(input...))
-	})
-}
-
-func TestPathExistings(t *testing.T) {
-	tempDirStr := t.TempDir()
-
-	fileName := "tempfile.lol"
-	existingFileStr := filepath.Join(tempDirStr, fileName)
-	existingFilePath := NewPath(existingFileStr)
-	file, err := os.OpenFile(existingFileStr, os.O_RDONLY|os.O_CREATE, 0666)
-	assert.NoError(t, err)
-
-	err = file.Close()
-	assert.NoError(t, err)
-
-	randomStr := "f8safkjh3asd09aslkja23shj87asds34"
-	nonExistingPathStr := filepath.Join(tempDirStr, randomStr)
-
-	tempDirPath := NewPath(tempDirStr)
-	nonExistingPath := NewPath(nonExistingPathStr)
-
-	// test temporary directory
-	t.Run("temporary directory", func(t *testing.T) {
-		assert.True(t, tempDirPath.Exists())
-		assert.True(t, tempDirPath.IsDir())
-		assert.False(t, tempDirPath.IsFile())
-	})
-
-	// exist existing file
-	t.Run("existing file", func(t *testing.T) {
-		assert.True(t, existingFilePath.Exists())
-		assert.False(t, existingFilePath.IsDir())
-		assert.True(t, existingFilePath.IsFile())
-	})
-
-	// test non-existing path
-	t.Run("non-existing file", func(t *testing.T) {
-		assert.False(t, nonExistingPath.Exists())
-		assert.False(t, nonExistingPath.IsDir())
-		assert.False(t, nonExistingPath.IsFile())
+		require.Equal(t, *expect, *PathFromParts(input...))
 	})
 }
 
@@ -266,7 +225,7 @@ func TestPath_Parent(t *testing.T) {
 	}
 
 	runForResults(t, cases, func(t *testing.T, input *Path, expect string) {
-		assert.Equal(t, expect, input.Parent().path)
+		require.Equal(t, expect, input.Parent().path)
 	})
 }
 
@@ -287,7 +246,7 @@ func TestPath_Parts(t *testing.T) {
 	}
 
 	runForResults(t, cases, func(t *testing.T, input *Path, expect []string) {
-		assert.Equal(t, expect, input.Parts())
+		require.Equal(t, expect, input.Parts())
 	})
 }
 
@@ -310,7 +269,7 @@ func TestPath_Base(t *testing.T) {
 	}
 
 	runForResults(t, cases, func(t *testing.T, input *Path, expect string) {
-		assert.Equal(t, expect, input.Base())
+		require.Equal(t, expect, input.Base())
 	})
 }
 
@@ -331,12 +290,12 @@ func TestPath_Split(t *testing.T) {
 	}
 
 	runForResults(t, cases, func(t *testing.T, input *Path, expect []string) {
-		assert.Equal(t, len(expect), 2)
+		require.Equal(t, len(expect), 2)
 
 		inputPartParent, inputPartBase := input.Split()
 
-		assert.Equal(t, expect[0], inputPartParent.path, "Parent")
-		assert.Equal(t, expect[1], inputPartBase, "Base")
+		require.Equal(t, expect[0], inputPartParent.path, "Parent")
+		require.Equal(t, expect[1], inputPartBase, "Base")
 	})
 }
 
@@ -375,7 +334,7 @@ func TestPath_Stem(t *testing.T) {
 	}
 
 	runForResults(t, cases, func(t *testing.T, input *Path, expect string) {
-		assert.Equal(t, expect, input.Stem())
+		require.Equal(t, expect, input.Stem())
 	})
 }
 
@@ -405,7 +364,7 @@ func TestPath_HasExtensions(t *testing.T) {
 	}
 
 	runForResults(t, cases, func(t *testing.T, input *Path, expect bool) {
-		assert.Equal(t, expect, input.HasExtensions())
+		require.Equal(t, expect, input.HasExtensions())
 	})
 }
 
@@ -435,7 +394,7 @@ func TestPath_Extension(t *testing.T) {
 	}
 
 	runForResults(t, cases, func(t *testing.T, input *Path, expect string) {
-		assert.Equal(t, expect, input.Extension())
+		require.Equal(t, expect, input.Extension())
 	})
 }
 
@@ -465,10 +424,10 @@ func TestPath_ExtensionParts(t *testing.T) {
 	}
 
 	runForResults(t, cases, func(t *testing.T, input *Path, expect []string) {
-		assert.Equal(t, expect, input.ExtensionParts())
+		require.Equal(t, expect, input.ExtensionParts())
 
 		// if the expected parts match, ExtensionCount should also be correct.
-		assert.Equal(t, len(expect), input.ExtensionCount())
+		require.Equal(t, len(expect), input.ExtensionCount())
 	})
 }
 
@@ -495,7 +454,7 @@ func TestPath_Root(t *testing.T) {
 	}
 
 	runForResults(t, cases, func(t *testing.T, input *Path, expect string) {
-		assert.Equal(t, expect, input.Root())
+		require.Equal(t, expect, input.Root())
 	})
 }
 
@@ -521,8 +480,8 @@ func TestPath_AbsoluteAndRelative(t *testing.T) {
 	}
 
 	runForResults(t, cases, func(t *testing.T, input *Path, expect bool) {
-		assert.Equal(t, expect, input.IsAbsolute())
-		assert.Equal(t, !expect, input.IsRelative())
+		require.Equal(t, expect, input.IsAbsolute())
+		require.Equal(t, !expect, input.IsRelative())
 	})
 }
 
@@ -546,22 +505,22 @@ func TestPath_RelativeTo(t *testing.T) {
 	}
 
 	runForResultsE(t, cases, func(t *testing.T, input []*Path, expect *Path, error bool) {
-		assert.Equal(t, len(input), 2)
+		require.Equal(t, len(input), 2)
 
 		basePath := input[0]
 		otherPath := input[1]
 		relativePath, err := basePath.RelativeTo(otherPath)
 
-		assert.Equal(t, error, err != nil)
+		require.Equal(t, error, err != nil)
 		if !error {
-			assert.Equal(t, expect, relativePath)
+			require.Equal(t, expect, relativePath)
 		}
 	})
 }
 
 func TestPath_Absolute(t *testing.T) {
 	wdPath, err := NewCwd()
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
 	cases := []TestCase[*Path, *Path]{
 		{Input: NewPath("."), Expect: wdPath.JoinStrings(".")},
@@ -577,9 +536,9 @@ func TestPath_Absolute(t *testing.T) {
 
 	runForResults(t, cases, func(t *testing.T, input *Path, expect *Path) {
 		absolutePath, err := input.Absolute()
-		assert.NoError(t, err)
+		require.NoError(t, err)
 
-		assert.Equal(t, expect, absolutePath)
+		require.Equal(t, expect, absolutePath)
 	})
 }
 
@@ -598,59 +557,15 @@ func TestPath_AbsoluteTo(t *testing.T) {
 	}
 
 	runForResultsE(t, cases, func(t *testing.T, input []*Path, expect *Path, error bool) {
-		assert.Equal(t, len(input), 2)
+		require.Equal(t, len(input), 2)
 
 		base := input[0]
 		other := input[1]
 		absolutePath, err := base.AbsoluteTo(other)
-		assert.Equal(t, error, err != nil)
+		require.Equal(t, error, err != nil)
 
 		if !error {
-			assert.Equal(t, expect, absolutePath)
-		}
-	})
-}
-
-func TestPath_Resolve(t *testing.T) {
-	// temporary directory
-	tempPath := NewPath(t.TempDir()) // creates temporary directory at /var
-
-	// temporary file paths
-	originalFile := "originalFile"
-	symlinkFile := "symlinkedFile"
-
-	originalFilePath := NewPath("/private").JoinStrings(tempPath.String(), originalFile) // /var is actually a symlink to /private/var
-	symlinkFilePath := tempPath.JoinStrings(symlinkFile)
-
-	// create original file
-	file, err := os.OpenFile(originalFilePath.String(), os.O_RDONLY|os.O_CREATE, 0666)
-	assert.NoError(t, err)
-
-	err = file.Close()
-	assert.NoError(t, err)
-
-	// create symlink
-	err = os.Symlink(originalFilePath.String(), symlinkFilePath.String())
-	assert.NoError(t, err)
-
-	cases := []TestCase[*Path, *Path]{
-		{Input: NewPath("."), Expect: NewPath(".")},
-		{Input: NewPath("./foo"), Error: true},
-		{Input: NewPath("/"), Expect: NewPath("/")},
-		{Input: originalFilePath, Expect: originalFilePath},
-		{Input: symlinkFilePath, Expect: originalFilePath},
-	}
-
-	for i, testCase := range cases {
-		cases[i].Name = fmt.Sprintf("[%s]", testCase.Input)
-	}
-
-	runForResultsE(t, cases, func(t *testing.T, input *Path, expect *Path, error bool) {
-		resolvedPath, err := input.Resolve()
-		assert.Equal(t, error, err != nil)
-
-		if !error {
-			assert.Equal(t, expect, resolvedPath)
+			require.Equal(t, expect, absolutePath)
 		}
 	})
 }
@@ -672,7 +587,7 @@ func TestPath_Joins(t *testing.T) {
 	}
 
 	runForResults(t, cases, func(t *testing.T, input []string, expect *Path) {
-		assert.True(t, len(input) > 0)
+		require.True(t, len(input) > 0)
 
 		basePath := NewPath(input[0])
 		joinedStrPath := basePath.JoinStrings(input[1:]...)
@@ -683,89 +598,8 @@ func TestPath_Joins(t *testing.T) {
 		}
 		joinedPathsPath := basePath.Join(strCvtPath...)
 
-		assert.Equal(t, expect, joinedStrPath)
-		assert.Equal(t, expect, joinedPathsPath)
-	})
-}
-
-func TestPath_GlobContains(t *testing.T) {
-
-	// NOTICE:
-	// This test only tests the existence check mechanism and
-	// matched filepath string conversions
-
-	// temporary directory
-	tempPath := NewPath(t.TempDir()) // creates temporary directory at /var
-
-	// create a file
-	existingFile := "foo"
-	existingFilePath := tempPath.JoinStrings(existingFile)
-	file, err := os.OpenFile(existingFilePath.String(), os.O_RDONLY|os.O_CREATE, 0666)
-	assert.NoError(t, err)
-
-	err = file.Close()
-	assert.NoError(t, err)
-
-	// create subdirectory
-	existingSubdirectory := "bar"
-	existingSubdirectoryPath := tempPath.JoinStrings(existingSubdirectory)
-	err = os.Mkdir(existingSubdirectoryPath.String(), 0777)
-	assert.NoError(t, err)
-
-	// create file in subdirectory
-	existingSubdirFile := "baz"
-	existingSubdirFilePath := existingSubdirectoryPath.JoinStrings(existingSubdirFile)
-	subdirFile, err := os.OpenFile(existingSubdirFilePath.String(), os.O_RDONLY|os.O_CREATE, 0666)
-	assert.NoError(t, err)
-
-	err = subdirFile.Close()
-	assert.NoError(t, err)
-
-	// test cases; the first string is the root path
-	// starting at the temporary directory, the second
-	// string is the pattern to search for
-
-	// TODO support double asterisks globbing, without adding dependencies
-	// see https://github.com/golang/go/issues/11862
-	cases := []TestCase[[]string, int]{
-		{Input: []string{"", ""}, Error: true},
-		{Input: []string{"", "  "}, Error: true},
-		{Input: []string{"", "  \t"}, Error: true},
-		{Input: []string{"", " \t \n  "}, Error: true},
-		{Input: []string{"", "*"}, Expect: 2},
-		{Input: []string{"", "/*"}, Expect: 2},
-		{Input: []string{"", "**"}, Expect: 2},
-		{Input: []string{"", "*/*"}, Expect: 1},
-		{Input: []string{"", "bar/*"}, Expect: 1},
-		{Input: []string{"", "bar/bar"}, Expect: 0},
-		{Input: []string{"", "bar/baz"}, Expect: 1},
-		{Input: []string{"", "bar/*z"}, Expect: 1},
-		{Input: []string{"", "bat/*z"}, Expect: 0},
-	}
-
-	for i, testCase := range cases {
-		cases[i].Name = fmt.Sprintf("[%s]", testCase.Input)
-	}
-
-	runForResultsE(t, cases, func(t *testing.T, input []string, expect int, error bool) {
-		assert.Len(t, input, 2)
-		assert.GreaterOrEqual(t, expect, 0)
-
-		path := tempPath.JoinStrings(input[0])
-		pattern := input[1]
-
-		matches, globErr := path.Glob(pattern)
-		contains, containsErr := path.Contains(pattern)
-		containsB := path.BContains(pattern)
-
-		assert.Equal(t, error, globErr != nil)
-		assert.Equal(t, error, containsErr != nil)
-
-		if !error {
-			assert.Len(t, matches, expect)
-			assert.Equal(t, expect != 0, contains)
-			assert.Equal(t, contains, containsB)
-		}
+		require.Equal(t, expect, joinedStrPath)
+		require.Equal(t, expect, joinedPathsPath)
 	})
 }
 
@@ -789,14 +623,14 @@ func TestPath_Equals(t *testing.T) {
 	}
 
 	runForResults(t, cases, func(t *testing.T, input []string, expect bool) {
-		assert.Len(t, input, 2)
+		require.Len(t, input, 2)
 
 		basePath := NewPath(input[0])
 		pathEquals := basePath.Equals(NewPath(input[1]))
 		stringEquals := basePath.EqualsString(input[1])
 
-		assert.Equal(t, expect, pathEquals)
-		assert.Equal(t, expect, stringEquals)
+		require.Equal(t, expect, pathEquals)
+		require.Equal(t, expect, stringEquals)
 	})
 }
 
@@ -820,14 +654,14 @@ func TestPath_EqualsFlat(t *testing.T) {
 	}
 
 	runForResults(t, cases, func(t *testing.T, input []string, expect bool) {
-		assert.Len(t, input, 2)
+		require.Len(t, input, 2)
 
 		basePath := NewPath(input[0])
 		pathEquals := basePath.EqualsFlat(NewPath(input[1]))
 		stringEquals := basePath.EqualsStringFlat(input[1])
 
-		assert.Equal(t, expect, pathEquals)
-		assert.Equal(t, expect, stringEquals)
+		require.Equal(t, expect, pathEquals)
+		require.Equal(t, expect, stringEquals)
 	})
 }
 
@@ -856,7 +690,7 @@ func TestPath_ToPosix(t *testing.T) {
 	runForResults(t, cases, func(t *testing.T, input *Path, expect string) {
 		toPosix := input.ToPosix()
 
-		assert.Equal(t, expect, toPosix)
+		require.Equal(t, expect, toPosix)
 	})
 }
 
@@ -878,13 +712,13 @@ func TestPath_WithName(t *testing.T) {
 	}
 
 	runForResults(t, cases, func(t *testing.T, input []string, expect *Path) {
-		assert.True(t, len(input) == 2)
+		require.True(t, len(input) == 2)
 
 		// call function and assert
 		path := NewPath(input[0])
 		changedName := path.WithName(input[1])
 
-		assert.Equal(t, expect, changedName)
+		require.Equal(t, expect, changedName)
 	})
 }
 
@@ -905,11 +739,11 @@ func TestPath_Copy(t *testing.T) {
 		copiedPath := input.Copy()
 
 		// compare pointers
-		assert.True(t, input == pointerCopy)
-		assert.False(t, input == copiedPath)
+		require.True(t, input == pointerCopy)
+		require.False(t, input == copiedPath)
 
 		// ensure copied path has same contents as original
-		assert.Equal(t, input, copiedPath)
+		require.Equal(t, input, copiedPath)
 	})
 }
 
@@ -932,7 +766,7 @@ func TestPath_flipCase(t *testing.T) {
 	}
 
 	runForResults(t, cases, func(t *testing.T, input string, expect string) {
-		assert.Equal(t, expect, flipCase(input))
+		require.Equal(t, expect, flipCase(input))
 	})
 }
 
